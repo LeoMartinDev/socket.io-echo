1b9e7f179f295d78da605cdab72090c15baa165a
leoMartinDev <leo1.martin@epitech.eu>
2020-10-05 17:44:40 +0200

    Base commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const socketIo = require('socket.io');
+
+module.exports = {
+    startServer,
+};
+
+function startServer({ port }) {
+    const io = socketIo(port);
+
+    io.on('connection', (socket) => {
+        socket.use((packet, next) => {
+            const eventName = packet.shift();
+
+            packet.pop();
+
+            const data = packet || [];
+
+            socket.emit(eventName, ...data);
+
+            next();
+          });
+    });
+
+    console.info(`Server is listening at port ${port}!`);
+}
